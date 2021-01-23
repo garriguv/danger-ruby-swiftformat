bbcecb6dfc2c14b492bbd8d4fe756466a418160b
Vincent Garrigues <vincent@garriguv.io>
2021-01-23 16:13:40 +0100

    Spring cleaning (#34)
    * Add GitHub workflow for tests
    
    * Bump dependencies

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require "pathname"
 ROOT = Pathname.new(File.expand_path("..", __dir__))
-$:.unshift((ROOT + "lib").to_s)
-$:.unshift((ROOT + "spec").to_s)
+$:.unshift("#{ROOT}lib".to_s)
+$:.unshift("#{ROOT}spec".to_s)
 
 require "bundler/setup"
 require "pry"
